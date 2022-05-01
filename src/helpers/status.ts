13fedeaa44b8db496bb9b0106aa44299160668a7
Alucard <minxter.yt@gmail.com>
2022-05-02 00:59:05 +0100

    Client schedular and better comments
    Client schedular for repetitive tasks now implemented using toad-schedular
    
    Better comments in index.ts and ready.ts
    
    Status now set every 10 minutes and on client ready event

---
+++ src/helpers/status.ts
@@ -0,0 +1,28 @@
+import { customClient } from "../index";
+
+export function refreshStatus(client: customClient){
+	if(client.isReady()){
+		try{ // @ts-ignore
+			client.user.setActivity('for h!help', {type: "WATCHING"})
+		} catch (e) {}
+	}
+}
+
+//const status = require('../data/status.json');
+
+// let types = [
+// 	"WATCHING"
+// ]
+
+// export function refreshStatus(client: customClient){
+// 	let type = types[Math.floor(Math.random() * types.length)];
+// 	let name = status[type][Math.floor(Math.random() * status[type].length)]
+// 	if(client.isReady()){
+// 		name = name
+// 			.replace('{servers}', client.guilds.cache.size + `${client.guilds.cache.size === 1 ? ' server' : ' servers'}`)
+// 			.replace('{members}', client.users.cache.size + `${client.users.cache.size === 1 ? ' member' : ' members'}`)
+// 		try{ // @ts-ignore
+// 			client.user.setActivity(name, {type: type})
+// 		} catch (e) {}
+// 	}
+// }
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,33 @@
+// Libraries
 import { Client, Guild, Intents } from 'discord.js';
+import { ToadScheduler, SimpleIntervalJob, Task } from "toad-scheduler";
 import { connect, Mongoose } from "mongoose";
-import { Command, commands } from './commands';
-import { Event, events } from './events/';
-import { servers } from "./helpers/server";
 require('dotenv').config()
 
+// Core
+import { Command, commands } from './commands';
+import { Event, events } from './events/';
+
+// Helpers
+import { servers } from "./helpers/server";
+
+// Custom client class
 export class customClient extends Client {
 	commands: Command[] = commands
 	prefix: string = 'h!'
 	owners: number[] = [719292655963734056, 778110527892488233]
 	db!: Mongoose
 	servers = servers
+	scheduler = new ToadScheduler()
 }
 
+// Client options
 let client = new customClient({
 	intents: [Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGE_REACTIONS],
 	partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
 });
 
+// Event Loader
 for (let event of events) {
 	if (event.once) {
 		client.once(event.name, (...args) => event.execute(...args, client));
@@ -26,5 +36,5 @@ for (let event of events) {
 	}
 }
 
-
+// Client login (Bot start)
 void client.login(process.env.TOKEN)
